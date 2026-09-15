81183d573c420000cc6256caba83aeff3c069851
kevylin <githublin@foxmail.com>
2026-09-15 16:53:48 +0800

    chore: 升级 pnpm 至 11.10.0、natapp 替换 ngrok，移除 fix_links 临时脚本

--- fix_links.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rmdir "g:\test\Nest\english\server\node_modules\alipay-sdk" 2>nul
-rmdir "g:\test\Nest\english\server\node_modules\socket.io" 2>nul
-mklink /J "g:\test\Nest\english\server\node_modules\alipay-sdk" "g:\test\Nest\english\node_modules\.pnpm\alipay-sdk@4.14.0\node_modules\alipay-sdk"
-mklink /J "g:\test\Nest\english\server\node_modules\socket.io" "g:\test\Nest\english\node_modules\.pnpm\socket.io@4.8.3\node_modules\socket.io"
-exit
